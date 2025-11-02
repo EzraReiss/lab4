3c25abed6ce94e25544cc9d3054a4b76b2e0e52b
er495 <er495@cornell.edu>
2025-11-02 18:42:31 -0500

    rename

--- ecelinux/run.tcl
+++ ecelinux/run.tcl
@@ -17,7 +17,7 @@ add_files bnn.cpp -cflags "-std=c++11"
 add_files -tb bnn_test.cpp -cflags "-std=c++11"
 add_files -tb data
 
-open_solution "solution1-conv--dense-reshape"
+open_solution "solution1-conv-dense-reshape"
 # Use Zynq device
 set_part {xc7z020clg484-1}
 
