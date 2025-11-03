37a94964f1cf9fad8232637e6fdc40c38df3642e
er495 <er495@cornell.edu>
2025-11-02 23:12:36 -0500

    linebuffer

--- ecelinux/layer.h
+++ ecelinux/layer.h
@@ -19,9 +19,12 @@
 template <int M, int I>
 void pad(bit input[M][I][I], bit output[M][I + F_PAD][I + F_PAD]) {
   #pragma HLS INLINE off
-  for (int m = 0; m < M; m++) {
-    for (int x = 0; x < I; x++) {
-      for (int y = 0; y < I; y++) {
+  #pragma HLS array_reshape variable = input complete dim = 1
+  #pragma HLS array_reshape variable = output complete dim = 1
+  for (int x = 0; x < I; x++) {
+    for (int y = 0; y < I; y++) {
+      #pragma HLS pipeline
+      for (int m = 0; m < M; m++) {
         output[m][y + F_PAD / 2][x + F_PAD / 2] = input[m][y][x];
       }
     }
@@ -36,40 +39,19 @@ void pad(bit input[M][I][I], bit output[M][I + F_PAD][I + F_PAD]) {
 template <int M, int I, int C>
 void initialize_padded_memory(bit input[M][I][I]) {
   #pragma HLS INLINE off
-  for (int m = 0; m < M; m++) {
-    for (int x = 0; x < I; x++) {
-      for (int y = 0; y < I; y++) {
+  #pragma HLS array_reshape variable = input complete dim = 1
+
+  for (int x = 0; x < I; x++) {
+    for (int y = 0; y < I; y++) {
+      #pragma HLS pipeline
+      for (int m = 0; m < M; m++) {
         input[m][x][y] = C;
       }
     }
   }
 }
 
-template <
-  typename T,
-  int M
-> 
-void initialize_line_buffer(
-  T line_buffer[M],
-  T initial_values[M]
-) {
-  for (int m = 0; m < M; m++) {
-    line_buffer[m] = initial_values[m];
-  }
-}
 
-template<
-  typename T,
-  int M
-> void shift_line_buffer(
-  T line_buffer[M],
-  T new_value
-) {
-  for (int m = M - 1; m > 0; m--) {
-    line_buffer[m] = line_buffer[m - 1];
-  }
-  line_buffer[0] = new_value;
-}
 //----------------------------------------------------------
 // Perform Convolution Layer
 //----------------------------------------------------------
@@ -89,73 +71,77 @@ void conv(
 ) {
   #pragma HLS INLINE off
 
-  bit input_slice[M][F][F];
+  // Adding BRAMs for the M dimension (input features)
+  #pragma HLS array_reshape variable=weight complete dim = 1
+  #pragma HLS array_reshape variable=input complete dim = 1
 
-    // Adding BRAMs for rows and columns using cyclic partitioning
-  #pragma HLS array_partition variable = weight complete dim = 4
-  #pragma HLS array_partition variable = weight complete dim = 3
+  // Adding BRAMs for rows and columns using cyclic partitioning
   #pragma HLS array_partition variable = weight complete dim = 2
-  #pragma HLS array_reshape variable = weight complete dim = 1
 
-  #pragma HLS array_partition variable = input_slice complete dim = 3
-  #pragma HLS array_partition variable = input_slice complete dim = 2
-  #pragma HLS array_reshape variable = input_slice complete dim = 1
+  #pragma HLS array_partition variable = threshold complete dim = 1
+  #pragma HLS array_partition variable=output complete dim = 1
 
-  #pragma HLS array_reshape variable = input complete dim = 1
+  constexpr int num_accum = F * F * M;
+  constexpr int lb_size = I * (F - 1) + F;
 
-  // Adding BRAMs for the N dimension (output features)
-  #pragma HLS array_partition variable = output complete dim = 1
-
-  int num_accum = F * F * M;
+  bit linebuffer[M][lb_size];
+  #pragma HLS array_reshape variable=linebuffer complete dim = 1
+  #pragma HLS array_partition variable=linebuffer complete dim = 2
 
 
-  for (int x = 0; x < I - F + 1; x++)
-  {
-    for (int c = 0; c < F; c++) {
-      for (int r = 0; r < F; r++) {
-        for (int m = 0; m < M; m++) {
-          #pragma HLS unroll
-          input_slice[m][r][c] = input[m][r][x + c];
-        }
+  INIT_LINEBUFFER_X: for (int x = 0; x < F - 1; x++) {
+    INIT_LINEBUFFER_Y: for (int y = 0; y < I; y++) {
+      INIT_LINEBUFFER_FEATURES: for (int m = 0; m < M; m++) {
+        #pragma HLS unroll
+        linebuffer[m][x * I + y + F - 1] = input[m][y][x];
+      }
+    }
+  }
+  
+  OUTPUT_X: for (int x = 0; x < I - F + 1; x++) {
+
+    SHIFT_LINE_BUF_X: for (int p = 0; p < lb_size - F + 1; p++) {
+      #pragma HLS unroll
+      for (int m = 0; m < M; m++) {
+        linebuffer[m][p] = linebuffer[m][p + F - 1];
       }
     }
 
-    for (int y = 0; y < I - F + 1; y++) {
-      for (int n = 0; n < N; n++) {
-        #pragma HLS pipeline
+    INIT_LINE_BUF_X: for (int p = 0; p < F - 1; p++) {
+      #pragma HLS unroll
+      for (int m = 0; m < M; m++) {
+        linebuffer[m][I * (F - 1) + 1 + p] = input[m][p][x + F - 1];
+      }
+    }
 
+    OUTPUT_Y: for (int y = 0; y < I - F + 1; y++) {      
+      SHIFT_LINE_BUF_Y: for (int p = 0; p < lb_size - 1; p++) {
+        for (int m = 0; m < M; m++) {
+          #pragma HLS unroll
+          linebuffer[m][p] = linebuffer[m][p + 1];
+        }
+      }
+
+      INIT_LINE_BUF_Y: for (int m = 0; m < M; m++) {
+        #pragma HLS unroll
+        linebuffer[m][lb_size - 1] = input[m][y + F - 1][x + F - 1];
+      }
+      
+      OUTPUT_FEATURES: for (int n = 0; n < N; n++) {
+        #pragma HLS pipeline
+      
         bit16_t accum = 0;
 
-        for (int c = 0; c < F; c++) {
-          for (int r = 0; r < F; r++) {
-            for (int m = 0; m < M; m++) {
-              accum += input_slice[m][r][c] == weight[m][n][r][c];
+        WEIGHT_COLS: for (int c = 0; c < F; c++) {
+          WEIGHT_ROWS: for (int r = 0; r < F; r++) {
+            INPUT_FEATURES: for (int m = 0; m < M; m++) {
+              accum += linebuffer[m][r * I + c] == weight[m][n][c][r];
             }
           }
         }
 
         accum = (accum << 1) - num_accum;
         output[n][y][x] = accum > threshold[n] ? 1 : 0;
-      }
-
-      // Shift input slice left by one column
-      for (int r = 0; r < F - 1; r++) {
-        #pragma HLS unroll
-        for (int c = 0; c < F; c++) {
-          #pragma HLS unroll
-          for (int m = 0; m < M; m++) {
-            #pragma HLS unroll
-            input_slice[m][r][c] = input_slice[m][r + 1][c];
-          }
-        }
-      }
-      
-      // Load new column into input slice
-      for (int c = 0; c < F; c++) {
-        for (int m = 0; m < M; m++) {
-          #pragma HLS unroll
-          input_slice[m][F - 1][c] = input[m][y + F][x + c];
-        }
       }   
     }
   }
@@ -171,17 +157,31 @@ void conv(
 template <int M, int I>
 void max_pool(bit input[M][I][I], bit output[M][I / 2][I / 2]) {
   #pragma HLS INLINE off
-  for (int m = 0; m < M; m++) {
-    for (int x = 0; x < I / 2; x++) {
-      for (int y = 0; y < I / 2; y++) {
-        bit max = 0;
-        for (int c = 0; c < 2; c++) {
-          for (int r = 0; r < 2; r++) {
-            if (input[m][2 * y + r][2 * x + c])
-              max = 1;
+
+  #pragma HLS array_reshape variable = input complete dim = 1
+  #pragma HLS array_reshape variable = output complete dim = 1
+
+  for (int x = 0; x < I / 2; x++) {
+    for (int y = 0; y < I / 2; y++) {
+      #pragma HLS pipeline
+
+      bit accum[M];
+      for (int m = 0; m < M; m++) {
+        accum[m] = 0;
+      }
+
+      for (int c = 0; c < 2; c++) {
+        for (int r = 0; r < 2; r++) {
+          for (int m = 0; m < M; m++) {
+            #pragma HLS unroll
+            accum[m] |= input[m][2 * y + r][2 * x + c];
           }
         }
-        output[m][y][x] = max;
+      }
+
+      for (int m = 0; m < M; m++) {
+        #pragma HLS unroll
+        output[m][y][x] = accum[m];
       }
     }
   }
@@ -194,6 +194,7 @@ void max_pool(bit input[M][I][I], bit output[M][I / 2][I / 2]) {
 // @param[out] : output - input famps of the first dense layer
 void flatten(bit input[O_CHANNEL2][O_WIDTH][O_WIDTH], bit output[I_UNITS1]) {
   #pragma HLS INLINE off
+
   for (int c = 0; c < O_CHANNEL2; c++) {
     for (int y = 0; y < O_WIDTH; y++) {
       for (int x = 0; x < O_WIDTH; x++) {
@@ -213,6 +214,7 @@ void flatten(bit input[O_CHANNEL2][O_WIDTH][O_WIDTH], bit output[I_UNITS1]) {
 template <int M> 
 void sign(bit16_t input[M], bit output[M]) {
   #pragma HLS INLINE off
+
   for (int m = 0; m < M; m++) {
     output[m] = (input[m] > 0) ? 1 : 0;
   }
@@ -253,6 +255,8 @@ void dense(bit input[M], bit16_t output[N], const bit weight[M][N]) {
   #pragma HLS array_reshape variable=weight complete dim=1
 
   for (int n = 0; n < N; n++) {
+    #pragma HLS pipeline
+  
     bit16_t accum = 0;
     for (int m = 0; m < M; m++) {
       #pragma HLS unroll
--- ecelinux/run.tcl
+++ ecelinux/run.tcl
@@ -17,7 +17,7 @@ add_files bnn.cpp -cflags "-std=c++11"
 add_files -tb bnn_test.cpp -cflags "-std=c++11"
 add_files -tb data
 
-open_solution "solution1-conv-v2-dense-reshape"
+open_solution "solution1-linebuffer-1"
 # Use Zynq device
 set_part {xc7z020clg484-1}
 
